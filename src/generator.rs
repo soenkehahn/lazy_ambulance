9668bd3c72e74899fad48c00c808b26b6acd8faf
Jon Distad <jon.distad@gmail.com>
2018-03-07 17:17:34 -0500

    Tried closures... don't think it'll work

--- src/generator.rs
+++ src/generator.rs
@@ -3,6 +3,7 @@ use ringbuf::RingBuf;
 use jack::*;
 
 use std::f32::consts::PI;
+use std::rc::Rc;
 
 pub trait Generator: Send {
     fn next_sample(&mut self, _client: &Client) -> f32;
@@ -11,7 +12,7 @@ pub trait Generator: Send {
 // * actual sound
 
 pub fn generator() -> impl Generator {
-    Delay::new(Sin::new(Add(Mult(Sin::new(1.0), 50.0), 350.0)))
+    Delay::new(sin(add(mult(sin(konst(1.0)), konst(50.0)), konst(350.0))))
 }
 
 pub struct Sin<G: Generator> {
@@ -78,4 +79,50 @@ impl<G: Generator> Generator for Delay<G> {
         self.buffer.push(sample);
         sample * 0.7 + old * 0.3
     }
+}
+
+pub struct Foo<'a> {
+    next_fn: Box<FnMut(&Client) -> f32 + Send + 'a>,
+}
+
+impl<'a> Foo<'a> {
+    fn next(&mut self, client: &Client) -> f32 {
+        (*self.next_fn)(client)
+    }
+}
+
+impl<'a> Generator for Foo<'a> {
+    fn next_sample(&mut self, client: &Client) -> f32 {
+        self.next(client)
+    }
+}
+
+pub fn konst<'a>(f: f32) -> Rc<Foo<'a>> {
+    Rc::new(Foo {
+        next_fn: Box::new(move |_| f),
+    })
+}
+
+pub fn sin<'a>(freq: Rc<Foo<'a>>) -> Foo<'a> {
+    let mut phase = 0.0f32;
+    Rc::new(Foo {
+        next_fn: Box::new(move |client| {
+            let result = phase.sin();
+            phase += freq.next(client) * 2.0 * PI / (client.sample_rate() as f32);
+            phase %= 2.0 * PI;
+            result
+        }),
+    })
+}
+
+pub fn add<'a>(lhs: Rc<Foo<'a>>, rhs: Rc<Foo<'a>>) -> Rc<Foo<'a>> {
+    Rc::new(Foo {
+        next_fn: Box::new(move |client| lhs.next(client) + rhs.next(client)),
+    })
+}
+
+pub fn mult<'a>(lhs: Rc<Foo<'a>>, rhs: Rc<Foo<'a>>) -> Rc<Foo<'a>> {
+    Rc::new(Foo {
+        next_fn: Box::new(move |client| lhs.next(client) * rhs.next(client)),
+    })
 }
